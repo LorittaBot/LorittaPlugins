02ade2b7d395702bfea67cb9150bf10d1c685823
Nightdavisao <kevinmaicon27@gmail.com>
2018-12-27 21:51:55 -0300

    registrar o comando pra eu ir dormir

--- ArtsyJoyLori/src/main/kotlin/net/perfectdreams/loritta/ArtsyJoyLori.kt
+++ ArtsyJoyLori/src/main/kotlin/net/perfectdreams/loritta/ArtsyJoyLori.kt
@@ -3,9 +3,13 @@ package net.perfectdreams.loritta
 import com.mrpowergamerbr.loritta.plugin.LorittaPlugin
 import net.perfectdreams.loritta.commands.images.AtendenteCommand
 import net.perfectdreams.loritta.commands.images.TristeRealidadeCommand
+import net.perfectdreams.loritta.commands.administration.DashboardCommand
 
 class ArtsyJoyLori : LorittaPlugin() {
     override fun onEnable() {
+        // ADMIN
+        registerCommand(DashboardCommand())
+        // IMAGES
         registerCommand(AtendenteCommand())
         registerCommand(TristeRealidadeCommand())
     }
