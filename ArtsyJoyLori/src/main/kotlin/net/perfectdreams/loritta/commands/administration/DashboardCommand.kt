78c44b8a1ecd8f483e22594bc9bde1cf0e8edcf2
Nightdavisao <kevinmaicon27@gmail.com>
2018-12-28 11:12:02 -0300

    indentaçao dentaçao

--- ArtsyJoyLori/src/main/kotlin/net/perfectdreams/loritta/commands/administration/DashboardCommand.kt
+++ ArtsyJoyLori/src/main/kotlin/net/perfectdreams/loritta/commands/administration/DashboardCommand.kt
@@ -12,42 +12,43 @@ import net.perfectdreams.commands.loritta.LorittaCommand
 import net.perfectdreams.commands.loritta.LorittaCommandContext
 
 class DashboardCommand : LorittaCommand(arrayOf("dashboard", "painel", "configurar", "config"), CommandCategory.ADMIN) {
-	override fun getDescription(locale: BaseLocale): String? {
-		return locale["commands.moderation.dashboard.description"]
-	}
+    override fun getDescription(locale: BaseLocale): String? {
+        return locale["commands.moderation.dashboard.description"]
+    }
 
-	override val canUseInPrivateChannel: Boolean = true
+    override val canUseInPrivateChannel: Boolean = true
 
-	@Subcommand
-	suspend fun root (context: LorittaCommandContext, locale: BaseLocale) {
+    @Subcommand
+    suspend fun root(context: LorittaCommandContext, locale: BaseLocale) {
 
-		val guild: String = context.guild.id.toString()
-		val dashboard = "${Loritta.config.websiteUrl}dashboard"
-		val url = "${dashboard}/configure/{$guild}"
+        val guild: String = context.guild.id.toString()
+        val dashboard = "${Loritta.config.websiteUrl}dashboard"
+        val url = "${dashboard}/configure/{$guild}"
 
-			/*
-			Se o comando for executado em guildas,
-			e o autor tem permissão de alterar configurações no Dashboard (ou tem permissão de Gerenciar servidor),
-			dê o url do dashboard diretamente pro servidor.
-			*/
+        /*
+        Se o comando for executado em guildas,
+        e o autor tem permissão de alterar configurações no Dashboard (ou tem permissão de Gerenciar servidor),
+        dê o url do dashboard diretamente pro servidor.
+        */
 
-			if (!context.isPrivateChannel && (context.lorittaUser.hasPermission(LorittaPermission.ALLOW_ACCESS_TO_DASHBOARD)) || context.guild.selfMember.hasPermission(Permission.MANAGE_SERVER)) {
-				context.reply(
-						LoriReply(
-								"Dashboard: {$url}",
-								"<:wumplus:388417805126467594>"
-						)
-				)
-			}
+        if (!context.isPrivateChannel && (context.lorittaUser.hasPermission(LorittaPermission.ALLOW_ACCESS_TO_DASHBOARD)) || context.guild.selfMember.hasPermission(Permission.MANAGE_SERVER)) {
+            context.reply(
+                    LoriReply(
+                            "Dashboard: {$url}",
+                            "<:wumplus:388417805126467594>"
+                    )
+            )
+        } else {
+            // Se o comando for executando em mensagem privada dê o negoco pra slecionar o servidor
+            context.reply(
+                    LoriReply(
+                            "Dashboard: {$dashboard}",
+                            "<:wumplus:388417805126467594>"
+                    )
+            )
 
-			else {
-				// Se o comando for executando em mensagem privada dê o negoco pra slecionar o servidor
-				context.reply(
-						LoriReply(
-								"Dashboard: {$dashboard}",
-								"<:wumplus:388417805126467594>"
-						)
-				)
-			}
-		}
-	}
+        }
+
+    }
+
+}
