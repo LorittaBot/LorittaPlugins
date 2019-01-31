6e0991ed44ea1890b7ab9f691816289b15cc1b83
Nightdavisao <kevinmaicon27@gmail.com>
2019-01-31 19:56:40 -0200

    Adicionar +roleinfo

--- ArtsyJoyLori/src/main/kotlin/net/perfectdreams/loritta/ArtsyJoyLori.kt
+++ ArtsyJoyLori/src/main/kotlin/net/perfectdreams/loritta/ArtsyJoyLori.kt
@@ -4,11 +4,14 @@ import com.mrpowergamerbr.loritta.plugin.LorittaPlugin
 import net.perfectdreams.loritta.commands.images.AtendenteCommand
 import net.perfectdreams.loritta.commands.images.TristeRealidadeCommand
 import net.perfectdreams.loritta.commands.administration.DashboardCommand
+import net.perfectdreams.loritta.commands.discord.RoleInfoCommand
 
 class ArtsyJoyLori : LorittaPlugin() {
     override fun onEnable() {
         // ADMIN
         registerCommand(DashboardCommand())
+        // DISCORD
+        registerCommand(RoleInfoCommand())
         // IMAGES
         registerCommand(AtendenteCommand())
         registerCommand(TristeRealidadeCommand())
