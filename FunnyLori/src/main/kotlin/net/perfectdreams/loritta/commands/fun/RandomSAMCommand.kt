2a048c2ead654a33a00262d9d00c9fde23d7c3a3
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-02-01 16:45:00 -0200

    Corrigir "+randomsam" pegando os memes da página no Instagram

--- FunnyLori/src/main/kotlin/net/perfectdreams/loritta/commands/fun/RandomSAMCommand.kt
+++ FunnyLori/src/main/kotlin/net/perfectdreams/loritta/commands/fun/RandomSAMCommand.kt
@@ -1,11 +1,20 @@
 package net.perfectdreams.loritta.commands.`fun`
 
+import com.github.kevinsawicki.http.HttpRequest
+import com.github.salomonbrys.kotson.array
+import com.github.salomonbrys.kotson.get
+import com.github.salomonbrys.kotson.obj
+import com.github.salomonbrys.kotson.string
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
 import com.mrpowergamerbr.loritta.Loritta
+import com.mrpowergamerbr.loritta.utils.jsonParser
 import com.mrpowergamerbr.loritta.utils.locale.BaseLocale
 import net.perfectdreams.commands.annotation.Subcommand
 import net.perfectdreams.loritta.api.commands.CommandCategory
 import net.perfectdreams.loritta.api.commands.LorittaCommand
 import net.perfectdreams.loritta.api.commands.LorittaCommandContext
+import org.jsoup.Jsoup
 import twitter4j.Query
 import twitter4j.Status
 import twitter4j.TwitterFactory
@@ -15,7 +24,7 @@ import java.util.*
 class RandomSAMCommand : LorittaCommand(arrayOf("randomsam", "randomsouthamericamemes", "rsam", "rsouthamericamemes"), category = CommandCategory.IMAGES) {
     companion object {
         var lastRequest = 0L
-        var cachedMemes = listOf<Status>()
+        var cachedMemes = listOf<JsonElement>()
     }
 
     override fun getDescription(locale: BaseLocale): String? {
@@ -24,31 +33,22 @@ class RandomSAMCommand : LorittaCommand(arrayOf("randomsam", "randomsouthamerica
 
     @Subcommand
     suspend fun root(context: LorittaCommandContext) {
-        val cb = ConfigurationBuilder()
-        cb.setDebugEnabled(true)
-            .setOAuthConsumerKey(Loritta.config.twitterConfig.oAuthConsumerKey)
-            .setOAuthConsumerSecret(Loritta.config.twitterConfig.oAuthConsumerSecret)
-            .setOAuthAccessToken(Loritta.config.twitterConfig.oAuthAccessToken)
-            .setOAuthAccessTokenSecret(Loritta.config.twitterConfig.oAuthAccessTokenSecret)
-
         if (System.currentTimeMillis() >= (lastRequest + 60_000)) {
-            val tf = TwitterFactory(cb.build())
-            val twitter = tf.instance
-            // Twitter só permite procurar, no máximo, 9 dias atrás (mais que isso não retorna nada!)
-            val pastDaysAgo = Calendar.getInstance()
-            val goBackDays = Loritta.RANDOM.nextInt(9, 365)
-            pastDaysAgo.add(Calendar.DAY_OF_MONTH, -goBackDays)
-            val futureDaysAgo = Calendar.getInstance()
-            futureDaysAgo.add(Calendar.DAY_OF_MONTH, (-goBackDays + 9))
+            val scripts = Jsoup.connect("https://www.instagram.com/southamericamemes/?hl=pt-br")
+                    .get()
+                    .getElementsByTag("script")
 
-            val query = Query("from:SoutAmericMemes since:${pastDaysAgo.get(Calendar.YEAR)}-${pastDaysAgo.get(Calendar.MONTH) + 1}-${pastDaysAgo.get(Calendar.DAY_OF_MONTH)} until:${futureDaysAgo.get(Calendar.YEAR)}-${futureDaysAgo.get(Calendar.MONTH) + 1}-${futureDaysAgo.get(Calendar.DAY_OF_MONTH)}")
-            val result = twitter.search(query)
+            val instagramData = scripts.first { it.html().startsWith("window._sharedData = ") }.html().substring(21).let { it.substring(0, it.length - 1) }
 
-            cachedMemes = result.tweets.filter { !it.isRetweeted && it.mediaEntities.isNotEmpty() }
+            println(instagramData)
+
+            val json = jsonParser.parse(instagramData).obj
+
+            cachedMemes = json["entry_data"]["ProfilePage"].array[0]["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"].array.toMutableList()
             lastRequest = System.currentTimeMillis()
         }
 
         val randomMeme = cachedMemes.random()
-        context.sendMessage("<:sam:383614103853203456> **|** " + context.getAsMention(true) + "Cópia não comédia! ${randomMeme.mediaEntities.first().mediaURLHttps}")
+        context.sendMessage("<:sam:383614103853203456> **|** " + context.getAsMention(true) + "Cópia não comédia! ${randomMeme["node"]["display_url"].string}")
     }
 }
