f88961e358044d8d6e3d1f1a9c1b49c129eaf0bb
kaikecarlos <caheasta@gmail.com>
2019-05-07 10:10:32 -0300

    Update AsciiCommand.kt

--- ArtsyJoyLori/src/main/kotlin/net/perfectdreams/loritta/commands/images/AsciiCommand.kt
+++ ArtsyJoyLori/src/main/kotlin/net/perfectdreams/loritta/commands/images/AsciiCommand.kt
@@ -14,62 +14,75 @@ class AsciiCommand : LorittaCommand(arrayOf("ascii"), CommandCategory.IMAGES) {
         return locale["commands.images.ascii.description"]
         // TODO: Locales
     }
+    fun imgtoasciiimg(oldImage: BufferedImage): BufferedImage {
+        val asciiArt =
+            imgtoascii(resizeimg(oldImage, (oldImage.width / 5.0).roundToInt(), (oldImage.height / 5.0).roundToInt()))
+        val width = (oldImage.width)
+        val height = (oldImage.height)
+        val newImage = BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB)
+        val newImageGraph = newImage.graphics
+        newImageGraph.color = Color.LIGHT_GRAY
+        newImageGraph.fillRect(0, 0, width, height)
+        newImageGraph.color = Color.BLACK
+        newImageGraph.font = Font(Font.MONOSPACED, Font.BOLD, 9)
+        val asciisplit = asciiArt.split("\n").toTypedArray()
+        var t: Int = 0
+        for (a in 0 until asciisplit.size) {
+            t = t + 5
+            newImageGraph.drawString(asciisplit[a], oldImage.minX / 2, oldImage.minY + t)
+        }
+        newImageGraph.dispose()
+        return newImage
+    }
+ 
     fun imgtoascii(img: BufferedImage): String {
         val sb = StringBuilder((img.width + 1) * img.height)
         for (y in 0 until img.height) {
-          if (sb.isNotEmpty()) sb.append("\n")
+            if (sb.isNotEmpty()) sb.append("\n")
             for (x in 0 until img.width) {
                 val pixelColor = Color(img.getRGB(x, y))
                 val gValue =
-                        pixelColor.red.toDouble() * 0.2989 + pixelColor.blue.toDouble() * 0.5870 + pixelColor.green.toDouble() * 0.1140
+                    pixelColor.red.toDouble() * 0.2989 + pixelColor.blue.toDouble() * 0.5870 + pixelColor.green.toDouble() * 0.1140
                 val s = strChar(gValue)
                 sb.append(s)
-                }
+            }
         }
         return sb.toString()
     }
-   
-    fun strChar(g:Double):String {
-        var str = " "
-        if (g >= 240)
-        {
-            str = " "
-        }
-        else if (g >= 210)
-        {
-            str = "."
-        }
-        else if (g >= 190)
-        {
-            str = "*"
-        }
-        else if (g >= 170)
-        {
-            str = "+"
-        }
-        else if (g >= 120)
-        {
-            str = "^"
-        }
-        else if (g >= 110)
-        {
-            str = "&"
-        }
-        else if (g >= 80)
-        {
-            str = "8"
-        }
-        else if (g >= 60)
-        {
-            str = "#"
-        }
-        else
-        {
-            str = "@"
-        }
-        return str
-    }
 
+    fun resizeimg(img: BufferedImage, width: Int, height: Int): BufferedImage {
+        val resizedImg = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+        val graphics = resizedImg.createGraphics()
+        graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR)
+        graphics.drawImage(img, 0, 0, width, height, null)
+        graphics.dispose()
+        return resizedImg
+    }
+ 
+ 
+fun strChar(g: Double): Char {
+    val str: Char
+    if (g >= 230.0) {
+        str = ' '
+    } else if (g >= 200.0) {
+        str = '.'
+    } else if (g >= 180.0) {
+        str = '*'
+    } else if (g >= 160.0) {
+        str = ':'
+    } else if (g >= 130.0) {
+        str = 'o'
+    } else if (g >= 100.0) {
+        str = '&'
+    } else if (g >= 70.0) {
+        str = '8'
+    } else if (g >= 50.0) {
+        str = '#'
+    } else {
+        str = '@'
+    }
+    return str
+}
     @Subcommand
     suspend fun root(context: LorittaCommandContext, locale: BaseLocale, args: Array<String>) {
         val image = context.getImageAt(0, 0, 0)
@@ -78,12 +91,9 @@ class AsciiCommand : LorittaCommand(arrayOf("ascii"), CommandCategory.IMAGES) {
             return
         }
         // Mensagem final
-        var fMessage : String = imgtoascii(image)
         
         // Vamos criar uma imagem e escrever o texto 
-        val newImage = BufferedImage(image.height, image.width, BufferedImage.TYPE_INT_ARGB)
-        val newImageGraph = newImage.graphics as Graphics2D
-        newImageGraph.drawString(fMessage, 0, 0)
+        val newImage = imgtoasciiimg(image)
 
         context.sendFile(newImage, "ascii.png", context.getAsMention(true))
 
