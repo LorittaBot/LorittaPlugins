1ece36c846eda2903f5c1a968bd1f1c8462b8ee9
MrPowerGamerBR <leonardomalaman@gmail.com>
2019-05-20 14:59:42 -0300

    Adicionar "+briggscover" (já que o servidor oficial do Drawn Mask aclamou por tal comando)

--- ArtsyJoyLori/src/main/kotlin/net/perfectdreams/loritta/ArtsyJoyLori.kt
+++ ArtsyJoyLori/src/main/kotlin/net/perfectdreams/loritta/ArtsyJoyLori.kt
@@ -2,28 +2,28 @@ package net.perfectdreams.loritta
 
 import com.mrpowergamerbr.loritta.plugin.LorittaPlugin
 import net.perfectdreams.loritta.commands.actions.*
-import net.perfectdreams.loritta.commands.images.AtendenteCommand
-import net.perfectdreams.loritta.commands.images.TristeRealidadeCommand
 import net.perfectdreams.loritta.commands.administration.DashboardCommand
 import net.perfectdreams.loritta.commands.administration.RenameChannelCommand
 import net.perfectdreams.loritta.commands.discord.RenameEmojiCommand
 import net.perfectdreams.loritta.commands.discord.RoleInfoCommand
-import net.perfectdreams.loritta.commands.images.AsciiCommand
-import net.perfectdreams.loritta.commands.images.DrawnWordCommand
+import net.perfectdreams.loritta.commands.images.*
 
 class ArtsyJoyLori : LorittaPlugin() {
     override fun onEnable() {
         // ADMIN
         registerCommand(DashboardCommand())
         registerCommand(RenameChannelCommand())
+
         // DISCORD
         registerCommand(RoleInfoCommand())
         registerCommand(RenameEmojiCommand())
         // IMAGES
+
         registerCommand(AtendenteCommand())
         registerCommand(TristeRealidadeCommand())
         registerCommand(AsciiCommand())
         registerCommand(DrawnWordCommand())
+        registerCommand(BriggsCapaCommand())
 
         // ACTIONS
         registerCommand(AttackCommand())
